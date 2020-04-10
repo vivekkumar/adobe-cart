183291016c33253460f88ae003ac7d6db80fb75e
Kumar Vivek <vivek.kumar@nagra.com>
2020-04-10 21:03:16 +0530

    Updates

---
+++ precache-manifest.c90adbcc8ceec91777fdd25908cadee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1e810d4e8e5f9d5c363013d27814ca",
+    "url": "/adobe-cart/index.html"
+  },
+  {
+    "revision": "3736a3765c56a7ffe966",
+    "url": "/adobe-cart/static/css/main.51f71a72.chunk.css"
+  },
+  {
+    "revision": "7215efcd46e7d128096b",
+    "url": "/adobe-cart/static/js/2.cf2ee78c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adobe-cart/static/js/2.cf2ee78c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3736a3765c56a7ffe966",
+    "url": "/adobe-cart/static/js/main.b65f7728.chunk.js"
+  },
+  {
+    "revision": "85d7b387423c251dd355",
+    "url": "/adobe-cart/static/js/runtime-main.639915b5.js"
+  }
+]);
